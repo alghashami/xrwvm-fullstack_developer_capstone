cef6e369373733ee694aa4428350e575e26b0a76
alghashami <alghashami2040@gmail.com>
2025-10-01 04:36:40 -0400

    Fix static pages URLs and get server running

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,13 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+from django.urls import path
+from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # المسار الفارغ الآن يذهب إلى index
+    path(route='', view=views.index, name='index'),
+    
+    # مسارات الصفحات الأخرى
+    path(route='about/', view=views.about, name='about'),
+    path(route='contact_us/', view=views.contact, name='contact'),
+]
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -63,3 +63,15 @@ def login_user(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+# Add these functions
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact_us.html')
