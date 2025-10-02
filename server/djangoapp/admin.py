b8a8678eea99f1d21ef297a1f023248a67fdea5b
alghashami <alghashami2040@gmail.com>
2025-10-02 04:29:13 -0400

    FIX: Final correction for car model in views.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,17 +3,19 @@ from .models import CarMake, CarModel
 
 # Register your models here.
 
-# CarModelInline class
-class CarModelInline(admin.StackedInline):
-    model = CarModel
-    extra = 1 # How many extra empty forms to show
-
-# CarMakeAdmin class
+# CarMakeAdmin class to customize the CarMake admin page
 class CarMakeAdmin(admin.ModelAdmin):
-    inlines = [CarModelInline]
-    list_display = ('name', 'description') # Fields to display in the list view
+    list_display = ('name', 'description')
     search_fields = ['name']
+
+# CarModelAdmin class to customize the CarModel admin page
+class CarModelAdmin(admin.ModelAdmin):
+    # لقد قمنا بإزالة 'type' من السطر التالي
+    list_display = ('name', 'car_make', 'year')
+    # وقمنا بإزالة 'type' من السطر التالي أيضًا
+    list_filter = ['car_make']
+    search_fields = ['name', 'car_make__name']
 
 # Register the models with their custom admin classes
 admin.site.register(CarMake, CarMakeAdmin)
-admin.site.register(CarModel)
+admin.site.register(CarModel, CarModelAdmin)
